f49e35e0a2c5247b373d11c232261d2a3829f201
Andrey <a.bessalov@gmail.com>
2023-02-11 20:47:24 +0300

    commit

--- test2.py
+++ test2.py
@@ -2,6 +2,7 @@ import json
 import os
 import pickle
 import sys
+from datetime import datetime as dt
 
 # import matplotlib.pyplot as plt
 # import numpy as np
@@ -10,27 +11,22 @@ import sys
 # from sklearn.linear_model import LogisticRegression
 
 
-def get_input(local=False):
-    if local:
-        print("Reading local file dataset_61_iris.csv")
-
-        return "dataset_61_iris.csv"
-
+def get_input():
     dids = os.getenv("DIDS", None)
-
     if not dids:
         print("No DIDs found in environment. Aborting.")
         return
-
     dids = json.loads(dids)
-
     for did in dids:
         filename = f"data/inputs/{did}/0"  # 0 for metadata service
         print(f"Reading asset file {filename}.")
-
         return filename
 
-
 if __name__ == "__main__":
-    local = len(sys.argv) == 2 and sys.argv[1] == "local"
     print('Hello world!')
+    print('File content:')
+    filename = get_input()
+    out = open(filename).read()
+    print(out)
+    
+    print(dt.now())
