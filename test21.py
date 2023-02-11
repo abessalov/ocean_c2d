6b9edceabfe64592fe9521fd90ebff1667e5ae48
Andrey <a.bessalov@gmail.com>
2023-02-11 20:55:11 +0300

    commit

---
+++ test21.py
@@ -0,0 +1,32 @@
+import json
+import os
+import pickle
+import sys
+from datetime import datetime as dt
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# from sklearn import preprocessing
+# from sklearn.linear_model import LogisticRegression
+
+
+def get_input():
+    dids = os.getenv("DIDS", None)
+    if not dids:
+        print("No DIDs found in environment. Aborting.")
+        return
+    dids = json.loads(dids)
+    for did in dids:
+        filename = f"data/inputs/{did}/0"  # 0 for metadata service
+        print(f"Reading asset file {filename}.")
+        return filename
+
+if __name__ == "__main__":
+    print('Hello world!')
+    print('File content:')
+    filename = get_input()
+    out = open(filename).read()
+    print(out)
+    
+    print(dt.now())
