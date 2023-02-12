87c402df188010d27c5d6afb0ae74a05d9eb6d7e
Andrey <a.bessalov@gmail.com>
2023-02-12 23:12:28 +0300

    commit

--- test31.py
+++ test31.py
@@ -22,9 +22,14 @@ def get_input():
         return filename
 
 if __name__ == "__main__":
+    
+    feats_read  = ['CONTAMINANT','DATA']
+    feats_vals  = ['01h','02h','03h','04h','05h','06h','07h','08h','09h','10h','11h','12h','13h','14h','15h','16h','17h','18h','19h','20h','21h','22h','23h','24h']
+
     print('Start date: ', dt.now())
     file_in = get_input()
-    df1 = pd.read_csv(file_in, nrows = 100)
+    df1 = pd.read_csv(file_in, usecols = feats_read + feats_vals, 
+                  dtype = {k: 'float32' for k in feats_vals})
     print(df1.shape)
     
     df11 = df1.groupby('NOM COMARCA').size().reset_index()
