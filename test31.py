1ea6cc6599794eb6fc1399ee72f64cbc5f92a38c
Andrey <a.bessalov@gmail.com>
2023-02-12 21:05:38 +0300

    commit

--- test31.py
+++ test31.py
@@ -24,8 +24,8 @@ def get_input():
 if __name__ == "__main__":
     print('Start date: ', dt.now())
     filename = get_input()
-    # df1 = pd.read_csv(filename)
-    # print(df1.shape)
+    df1 = pd.read_csv(filename)
+    print(df1.shape)
     # df1.groupby('NOM COMARCA').size().reset_index().to_csv('out.csv', index = False)
     print('End date: ', dt.now())
     
