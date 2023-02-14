85dcb9f35848e2f54d88cd5c9734dfba0a0c51ee
Andrey <a.bessalov@gmail.com>
2023-02-14 12:34:50 +0300

    commit

--- test32.py
+++ test32.py
@@ -73,8 +73,8 @@ if __name__ == "__main__":
     
     df1 = get_data(get_input(), pollutant = 'O3')
     df_out = get_predictions(df1, t1 = 24)
-    file_out = "/data/outputs/result" 
-    df1.to_csv(file_out, index = False)
+    file_out = "/data/outputs/result.csv" 
+    df_out.to_csv(file_out, index = False)
     
     print('End date: ', dt.now())
     
