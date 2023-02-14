26121aeacd3d59d384a835537a4f6769be3e4389
Andrey <a.bessalov@gmail.com>
2023-02-14 18:01:01 +0300

    commit

--- test33.py
+++ test33.py
@@ -34,15 +34,15 @@ def get_data(file_in, pollutant = 'O3'):
     feats_read  = ['CODI EOI','CONTAMINANT','DATA']
     feats_vals  = ['01h','02h','03h','04h','05h','06h','07h','08h','09h','10h','11h','12h','13h','14h','15h','16h','17h','18h','19h','20h','21h','22h','23h','24h']
     df = pd.read_csv(file_in, usecols = feats_read + feats_vals, dtype = {k: 'float32' for k in feats_vals})
-    filt = (df.CONTAMINANT == pollutant) & (df.year >= 2013)
-    df = df[filt]
     
     # 2) preprocessing
     df = df.drop_duplicates(subset = ['CODI EOI','DATA','CONTAMINANT'])
     df['DATA'] = pd.to_datetime(df['DATA'], dayfirst = True)
+    df['year'] = df.DATA.dt.year
+    filt = (df.CONTAMINANT == pollutant) & (df.year >= 2013)
+    df = df[filt]
 
     # 3) calculate averages by the hour
-
     feats1 = ['DATA','CONTAMINANT']
     df1 = df.groupby(feats1)[feats_vals].mean()
     df1 = df1.stack().reset_index().rename(columns = {0:'val','level_2':'hour'})
