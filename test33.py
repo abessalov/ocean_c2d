21746305dbe0777e261c3e1ccfd3e1d86d4a55d1
Andrey <a.bessalov@gmail.com>
2023-02-14 19:24:53 +0300

    commit

--- test33.py
+++ test33.py
@@ -151,21 +151,8 @@ def get_predictions(x,y, t1 = 24*14):
 if __name__ == "__main__":
     print('Start date: ', dt.now())
     
-    # file_in = get_input()
-    # df1 = get_data(file_in, pollutant = 'O3')
-    
-    # 1) read data with memory optimizing
-    feats_read  = ['CODI EOI','CONTAMINANT','DATA']
-    feats_vals  = ['01h','02h','03h','04h','05h','06h','07h','08h','09h','10h','11h','12h','13h','14h','15h','16h','17h','18h','19h','20h','21h','22h','23h','24h']
-    df = pd.read_csv(file_in, usecols = feats_read + feats_vals, dtype = {k: 'float32' for k in feats_vals})
-
-    # 2) preprocessing
-    df = df.drop_duplicates(subset = ['CODI EOI','DATA','CONTAMINANT'])
-    df['DATA'] = pd.to_datetime(df['DATA'], dayfirst = True)
-    df['year'] = df.DATA.dt.year
-    filt = (df.CONTAMINANT == pollutant) & (df.year >= 2013)
-    df = df[filt]
-    print(df.shape)
+    file_in = get_input()
+    x,y = get_data(file_in, pollutant = 'O3')
     
     # df_out = get_predictions(df1, t1 = 24)
     # file_out = "/data/outputs/result.csv" 
