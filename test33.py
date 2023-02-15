cfcee8c58f05fbe5389802dccf43a6f96a4a1452
Andrey <a.bessalov@gmail.com>
2023-02-15 05:20:44 +0300

    commit

--- test33.py
+++ test33.py
@@ -11,7 +11,7 @@ from datetime import timedelta
 # install packages
 subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "wheel"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "lightgbm"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-u", "numpy","--upgrade"])
 
 # subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy","--upgrade"])
 # subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas","--upgrade"])
